c023b71b4f69cad3414683c5bcde72332943611c
Poompong B <62987723+Poompong-b@users.noreply.github.com>
2021-10-13 11:30:12 +0700

    LAB07 Finished

---
+++ SoftArchDesign/src/main/java/creational/BookMetadataExporter.java
@@ -0,0 +1,21 @@
+package creational;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.PrintStream;
+
+public abstract class BookMetadataExporter extends BookCollection {
+
+    public void export(PrintStream stream) throws IOException, ParserConfigurationException {
+        // Please implement this method. You may create additional methods as you see fit.
+
+        BookMetadataFormatter formatter =  createBookMetadataFormatter();
+        for (Book i : books
+        ) {
+            formatter.append(i);
+        }
+        stream.print(formatter.getMetadataString());
+    }
+
+    public abstract BookMetadataFormatter createBookMetadataFormatter() throws IOException, ParserConfigurationException;
+}
---
+++ SoftArchDesign/src/main/java/creational/JSONBookMetadataFormatter.java
@@ -0,0 +1,45 @@
+package creational;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class JSONBookMetadataFormatter implements BookMetadataFormatter {
+
+    JSONArray books;
+    String rootKey;
+
+    public JSONBookMetadataFormatter() {
+        reset();
+    }
+
+    public BookMetadataFormatter reset() {
+        // Please implement this method. You may create additional methods as you see fit.
+        books = new JSONArray();
+        rootKey = Book.class.getSimpleName() + "s";
+        return this;
+    }
+
+    public BookMetadataFormatter append(Book b) {
+        // Please implement this method. You may create additional methods as you see fit.
+        JSONObject bookDetail = new JSONObject();
+        bookDetail.put(Book.Metadata.ISBN.value, b.getISBN());
+        bookDetail.put(Book.Metadata.AUTHORS.value, Arrays.stream(b.getAuthors()).toList());
+        bookDetail.put(Book.Metadata.TITLE.value, b.getTitle());
+        bookDetail.put(Book.Metadata.PUBLISHER.value, b.getPublisher());
+
+        books.add(bookDetail);
+        return this;
+    }
+
+    public String getMetadataString() {
+        // Please implement this method. You may create additional methods as you see fit.
+        JSONObject jsonPrinter = new JSONObject();
+        jsonPrinter.put(rootKey, books);
+        return jsonPrinter.toString();
+    }
+}
