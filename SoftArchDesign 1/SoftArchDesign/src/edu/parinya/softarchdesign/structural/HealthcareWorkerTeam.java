0ef5b13e085f99c405ba2330649d92223ccc1b1f
Poompong B <62987723+Poompong-b@users.noreply.github.com>
2021-10-21 14:19:38 +0700

    SAD Structural Design LAB

---
+++ SoftArchDesign 1/SoftArchDesign/src/edu/parinya/softarchdesign/structural/HealthcareWorkerTeam.java
@@ -0,0 +1,29 @@
+package edu.parinya.softarchdesign.structural;
+import java.util.Set;
+import java.util.LinkedHashSet;
+
+public class HealthcareWorkerTeam implements HealthcareServiceable {
+    private Set<HealthcareServiceable> members = new LinkedHashSet<>();
+
+    public void addMember(HealthcareServiceable member) {
+        this.members.add(member);
+    }
+
+    public void removeMember(HealthcareServiceable member) {
+        this.members.remove(member);
+    }
+
+    @Override
+    public void service() {
+        for(HealthcareServiceable member: this.members)
+            member.service();
+    }
+
+    @Override
+    public double getPrice() {
+        double price = 0;
+        for(HealthcareServiceable member: this.members)
+            price += member.getPrice();
+        return price;
+    }
+}
